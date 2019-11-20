c660dc41b43cba27c2b18c5a9cefd8574e3015b8
arvindwaiker <arvindwaiker@gmail.com>
2019-11-20 19:45:29 +0530

    Selection Sort - Initial Commit

---
+++ src/main/java/io/awklabs/sorting/selectionsort/SelectionSort.java
@@ -0,0 +1,18 @@
+package io.awklabs.sorting.selectionsort;
+
+import io.awklabs.sorting.Sort;
+
+import java.util.Comparator;
+
+public class SelectionSort<T> implements Sort<T> {
+
+    @Override
+    public void sort(int[] array) {
+
+    }
+
+    @Override
+    public void sort(T[] array, Comparator<? super T> c) {
+
+    }
+}
---
+++ src/main/java/io/awklabs/sorting/selectionsort/SelectionSort.md
@@ -0,0 +1,3 @@
+Selection Sort
+
+https://www.youtube.com/watch?v=xWBP4lzkoyM
