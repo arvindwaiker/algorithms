e7509a99f34ed99a8ad84b46fcfaf6490f3e72a1
arvindwaiker <arvindwaiker@gmail.com>
2019-12-01 12:27:12 +0530

    Binary Search Tree Delete

--- src/main/java/io/awklabs/datastructures/binarytree/BinarySearchTree.java
+++ src/main/java/io/awklabs/datastructures/binarytree/BinarySearchTree.java
@@ -34,6 +34,41 @@ public class BinarySearchTree<T> {
         }
     }
 
+    public Node<T> delete(Node<T> root, T element, Comparator<? super T> c) {
+
+        if (root == null) {
+            return root;
+        }
+
+        if (c.compare(root.element, element) > 0) {
+            root.left = delete(root.left, element, c);
+        } else if (c.compare(root.element, element) < 0) {
+            root.right = delete(root.right, element, c);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            //Do not actually delete the node. Instead replace it with min value. Delete the min value node in the right sub tree.
+            root.element = (T) minValue(root.right, c);
+            root.right = delete(root.right, root.element, c);
+        }
+
+        return root;
+
+    }
+
+    private T minValue(Node<T> root, Comparator<? super T> c) {
+        T minv = root.element;
+        while (root.left != null) {
+            minv = (T) root.left.element;
+            root = root.left;
+        }
+        return minv;
+    }
+
     public void inOrder(Node<T> node, List<T> resultArray) {
 
         if (node == null) {
--- src/test/java/io/awklabs/datastructures/binarytree/BinarySearchTreeTest.java
+++ src/test/java/io/awklabs/datastructures/binarytree/BinarySearchTreeTest.java
@@ -41,4 +41,39 @@ public class BinarySearchTreeTest extends DataStructuresTestBase {
         assertEquals(Integer.valueOf(3), binarySearchTree.search(root, 3, Integer::compareTo).element);
         assertNull(binarySearchTree.search(root, 20, Integer::compareTo));
     }
+
+    @Test
+    public void testBSTDelete() {
+        Integer[] data = {9, 4, 10, 2, 1, 3, 6, 5, 7, 8};
+        Node<Integer> root = null;
+        for (int element : data) {
+            root = binarySearchTree.insert(root, element, Integer::compareTo);
+        }
+        Integer[] expected = {1, 2, 3, 5, 6, 7, 8, 9, 10};
+        binarySearchTree.delete(root, 4, Integer::compareTo);
+        List<Integer> result = new ArrayList<>();
+        binarySearchTree.inOrder(root, result);
+        assertArrayEquals(expected, result.toArray());
+
+
+    }
+
+    @Test
+    public void testBSTDeleteRight() {
+        Integer[] data = {9, 4, 2, 1, 3, 6, 5, 8, 7, 13, 12, 14, 11, 10};
+        Node<Integer> root = null;
+        for (int element : data) {
+            root = binarySearchTree.insert(root, element, Integer::compareTo);
+        }
+        Integer[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14};
+        binarySearchTree.delete(root, 11, Integer::compareTo);
+        List<Integer> result = new ArrayList<>();
+        binarySearchTree.inOrder(root, result);
+        assertArrayEquals(expected, result.toArray());
+    }
+
+    @Test
+    public void testBSTDeleteNull() {
+        assertNull(binarySearchTree.delete(null, 10, Integer::compareTo));
+    }
 }
