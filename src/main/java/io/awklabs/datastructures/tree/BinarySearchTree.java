f670008cfa23e40b970985f61f1e874d440151b2
arvindwaiker <arvindwaiker@gmail.com>
2019-12-02 16:05:12 +0530

    Binary Search Tree refactoring

--- src/main/java/io/awklabs/datastructures/tree/BinarySearchTree.java
+++ src/main/java/io/awklabs/datastructures/tree/BinarySearchTree.java
@@ -1,49 +1,56 @@
 package io.awklabs.datastructures.tree;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class BinarySearchTree<T> {
 
-    public Node<T> insert(Node<T> root, T element, Comparator<? super T> c) {
+    private Comparator<? super T> comparator;
+
+    public BinarySearchTree(Comparator<? super T> comparator) {
+        this.comparator = comparator;
+    }
+
+    public Node<T> insert(Node<T> root, T element) {
         assert element != null;
 
         if (root == null) {
             return new Node<>(element);
         }
 
-        if (c.compare(root.element, element) > 0) {
-            root.left = insert(root.left, element, c);
-        } else if (c.compare(root.element, element) < 0) {
-            root.right = insert(root.right, element, c);
+        if (comparator.compare(root.element, element) > 0) {
+            root.left = insert(root.left, element);
+        } else if (comparator.compare(root.element, element) < 0) {
+            root.right = insert(root.right, element);
         }
         return root;
     }
 
-    public Node<T> search(Node<T> root, T element, Comparator<? super T> c) {
+    public Node<T> search(Node<T> root, T element) {
         assert element != null;
 
-        if (root == null || c.compare(root.element, element) == 0) {
+        if (root == null || comparator.compare(root.element, element) == 0) {
             return root;
         }
 
-        if (c.compare(root.element, element) > 0) {
-            return search(root.left, element, c);
+        if (comparator.compare(root.element, element) > 0) {
+            return search(root.left, element);
         } else {
-            return search(root.right, element, c);
+            return search(root.right, element);
         }
     }
 
-    public Node<T> delete(Node<T> root, T element, Comparator<? super T> c) {
+    public Node<T> delete(Node<T> root, T element) {
 
         if (root == null) {
             return root;
         }
 
-        if (c.compare(root.element, element) > 0) {
-            root.left = delete(root.left, element, c);
-        } else if (c.compare(root.element, element) < 0) {
-            root.right = delete(root.right, element, c);
+        if (comparator.compare(root.element, element) > 0) {
+            root.left = delete(root.left, element);
+        } else if (comparator.compare(root.element, element) < 0) {
+            root.right = delete(root.right, element);
         } else {
             if (root.left == null) {
                 return root.right;
@@ -52,15 +59,15 @@ public class BinarySearchTree<T> {
             }
 
             //Do not actually delete the node. Instead replace it with min value. Delete the min value node in the right sub tree.
-            root.element = (T) minValue(root.right, c);
-            root.right = delete(root.right, root.element, c);
+            root.element = (T) minValue(root.right);
+            root.right = delete(root.right, root.element);
         }
 
         return root;
 
     }
 
-    private T minValue(Node<T> root, Comparator<? super T> c) {
+    private T minValue(Node<T> root) {
         T minv = root.element;
         while (root.left != null) {
             minv = (T) root.left.element;
@@ -79,4 +86,53 @@ public class BinarySearchTree<T> {
         resultArray.add(node.element);
         inOrder(node.right, resultArray);
     }
+
+    public Node<T> balance(Node<T> node) {
+
+        if (node == null || (node.left == null && node.right == null)) {
+            return node;
+        }
+
+        List<T> result = new ArrayList<>();
+
+        inOrder(node, result);
+
+        return insert((T[]) result.toArray(), 0, result.size() - 1, null);
+
+    }
+
+    private Node<T> insert(T[] array, int low, int high, Node<T> root) {
+
+        if (low == high) {
+            root = insert(root, array[low]);
+            return root;
+        } else if (low > high) {
+            return root;
+        }
+
+        int mid = (low + high) / 2;
+        root = insert(root, array[mid]);
+        root.left = insert(array, low, mid - 1, root.left);
+        root.right = insert(array, mid + 1, high, root.right);
+
+        return root;
+    }
+
+    public int height(Node<T> root) {
+
+        if (root == null) {
+            return 0;
+        } else {
+
+            int l = height(root.left);
+            int r = height(root.right);
+            if (l > r) {
+                return l + 1;
+            } else {
+                return r + 1;
+            }
+        }
+    }
+
+
 }
--- src/test/java/io/awklabs/datastructures/tree/BinarySearchTreeTest.java
+++ src/test/java/io/awklabs/datastructures/tree/BinarySearchTreeTest.java
@@ -15,14 +15,14 @@ public class BinarySearchTreeTest extends DataStructuresTestBase {
     @Override
     public void before() {
         super.before();
-        binarySearchTree = new BinarySearchTree<>();
+        binarySearchTree = new BinarySearchTree<>(Integer::compareTo);
     }
 
     @Test
     public void testBSTInsert() {
         Node<Integer> root = null;
         for (int element : data) {
-            root = binarySearchTree.insert(root, element, Integer::compareTo);
+            root = binarySearchTree.insert(root, element);
         }
         Integer[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         List<Integer> result = new ArrayList<>();
@@ -34,12 +34,12 @@ public class BinarySearchTreeTest extends DataStructuresTestBase {
     public void testBSTSearch() {
         Node<Integer> root = null;
         for (int element : data) {
-            root = binarySearchTree.insert(root, element, Integer::compareTo);
+            root = binarySearchTree.insert(root, element);
         }
 
-        assertEquals(Integer.valueOf(10), binarySearchTree.search(root, 10, Integer::compareTo).element);
-        assertEquals(Integer.valueOf(3), binarySearchTree.search(root, 3, Integer::compareTo).element);
-        assertNull(binarySearchTree.search(root, 20, Integer::compareTo));
+        assertEquals(Integer.valueOf(10), binarySearchTree.search(root, 10).element);
+        assertEquals(Integer.valueOf(3), binarySearchTree.search(root, 3).element);
+        assertNull(binarySearchTree.search(root, 20));
     }
 
     @Test
@@ -47,10 +47,10 @@ public class BinarySearchTreeTest extends DataStructuresTestBase {
         Integer[] data = {9, 4, 10, 2, 1, 3, 6, 5, 7, 8};
         Node<Integer> root = null;
         for (int element : data) {
-            root = binarySearchTree.insert(root, element, Integer::compareTo);
+            root = binarySearchTree.insert(root, element);
         }
         Integer[] expected = {1, 2, 3, 5, 6, 7, 8, 9, 10};
-        binarySearchTree.delete(root, 4, Integer::compareTo);
+        binarySearchTree.delete(root, 4);
         List<Integer> result = new ArrayList<>();
         binarySearchTree.inOrder(root, result);
         assertArrayEquals(expected, result.toArray());
@@ -63,10 +63,10 @@ public class BinarySearchTreeTest extends DataStructuresTestBase {
         Integer[] data = {9, 4, 2, 1, 3, 6, 5, 8, 7, 13, 12, 14, 11, 10};
         Node<Integer> root = null;
         for (int element : data) {
-            root = binarySearchTree.insert(root, element, Integer::compareTo);
+            root = binarySearchTree.insert(root, element);
         }
         Integer[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14};
-        binarySearchTree.delete(root, 11, Integer::compareTo);
+        binarySearchTree.delete(root, 11);
         List<Integer> result = new ArrayList<>();
         binarySearchTree.inOrder(root, result);
         assertArrayEquals(expected, result.toArray());
@@ -74,6 +74,22 @@ public class BinarySearchTreeTest extends DataStructuresTestBase {
 
     @Test
     public void testBSTDeleteNull() {
-        assertNull(binarySearchTree.delete(null, 10, Integer::compareTo));
+        assertNull(binarySearchTree.delete(null, 10));
+    }
+
+    @Test
+    public void testBalance() {
+        Node<Integer> root = null;
+        for (int element : data) {
+            root = binarySearchTree.insert(root, element);
+        }
+        assertEquals(6, binarySearchTree.height(root));
+        root = binarySearchTree.balance(root);
+        List<Integer> result = new ArrayList<>();
+        binarySearchTree.inOrder(root, result);
+        assertEquals(Integer.valueOf(5), root.element);
+        Integer[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        assertArrayEquals(expected, result.toArray());
+        assertEquals(4, binarySearchTree.height(root));
     }
 }
